629b758326f22b07d1851d9bac693580feac0cfc
wojtko <j.w2001@onet.pl>
2024-02-13 22:41:52 +0100

    Create page for a single blog's article

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,3 +1,26 @@
-export default function BlogArticle({ params }: { params: { slug: string } }) {
+import { FullBlog } from "@/lib/interface";
+import { client } from "@/lib/sanity";
+
+async function getData(slug: string) {
+  const query = `*[_type == "blog" && slug.current == "${slug}"] {
+        "currentSlug": slug.current,
+        title,
+        content, 
+        titleImage
+      }[0]`;
+
+  const data = await client.fetch(query);
+
+  return data;
+}
+
+export default async function BlogArticle({
+  params,
+}: {
+  params: { slug: string };
+}) {
+  const data: FullBlog = await getData(params.slug);
+  console.log(data);
+
   return <h1>{params.slug}</h1>;
 }
--- lib/interface.ts
+++ lib/interface.ts
@@ -1,6 +1,13 @@
 export interface BlogCard {
     title: string;
     description: string;
-    titleImage: string;
+    titleImage: any;
+    currentSlug: string;
+}
+
+export interface FullBlog {
+    title: string;
+    content: any;
+    titleImage: any;
     currentSlug: string;
 }
