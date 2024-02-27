5fb3d588ad2703b21f6ea3a825fbce0be4dff5fe
wojtko <j.w2001@onet.pl>
2024-02-27 22:31:27 +0100

    Improve blog page. Create sidebar.

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -4,6 +4,7 @@ import { PortableText } from "@portabletext/react";
 import Image from "next/image";
 import "../../globals.css";
 import Footer from "@/app/components/Footer";
+import { Facebook, Linkedin, Twitter, X } from "lucide-react";
 
 export const revalidate = 30;
 
@@ -28,7 +29,7 @@ export default async function BlogArticle({
   const data: FullBlog = await fetchArticleData(params.slug);
 
   return (
-    <div className="">
+    <div>
       <div className="mt-8 max-w-4xl w-full mx-auto">
         <h1 className="flex flex-col gap-2">
           <span className="text-base text-center text-primary tracking-wide font-semibold rounded-lg">
@@ -37,7 +38,7 @@ export default async function BlogArticle({
           <span className="mt-2 block text-3xl text-center leading-8 font-bold tracking-tight sm:text-4xl">
             {data.title}
           </span>
-          <span className="text-center dark:text-gray-300 text-gray-600">
+          <span className="text-center dark:text-gray-400 text-gray-600">
             6 mins read
           </span>
         </h1>
@@ -51,10 +52,28 @@ export default async function BlogArticle({
           priority
         />
 
-        <div className="my-8 prose prose-blue prose-xl dark:prose-invert">
-          <PortableText value={data.content} />
+        <div className="flex gap-4 relative my-8">
+          <div className="flex flex-col gap-1 sticky top-0 py-2 left-0 h-32 text-base border-t-2 border-dark dark:border-lightBackground">
+            <span className="">Written by</span>
+            <span className="font-semibold">@Jakub Wojtkowski</span>
+            <span>Feb 21, 2024</span>
+          </div>
+
+          <div className="prose prose-blue prose-xl dark:prose-invert pl-16">
+            <PortableText value={data.content} />
+
+            <div className="flex flex-col gap-2">
+              <span className="text-base">( Share )</span>
+              <div className="flex gap-2">
+                <Linkedin />
+                <Twitter />
+                <Facebook />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
+
       <Footer />
     </div>
   );
