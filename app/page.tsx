0568ea84eb0326bf93ba4e68515ee22517b09a90
wojtko <j.w2001@onet.pl>
2024-02-29 22:32:39 +0100

    Add transition to menu bar

--- app/page.tsx
+++ app/page.tsx
@@ -38,10 +38,18 @@ export default async function Home() {
         </h2>
 
         <ul className="flex flex-1 justify-center items-center gap-4 text-base">
-          <li>all</li>
-          <li>nutrition</li>
-          <li>self-development</li>
-          <li>mental health</li>
+          <li className="after:content-[''] after:absolute after:bg-light after:h-[3px] after:rounded-lg after:w-0 left-0 after:bottom-[-10px] hover:after:w-full after:transition-[500ms] cursor-pointer relative">
+            all
+          </li>
+          <li className="after:content-[''] after:absolute after:bg-light after:h-[3px] after:rounded-lg after:w-0 left-0 after:bottom-[-10px] hover:after:w-full after:transition-[500ms] cursor-pointer relative">
+            nutrition
+          </li>
+          <li className="after:content-[''] after:absolute after:bg-light after:h-[3px] after:rounded-lg after:w-0 left-0 after:bottom-[-10px] hover:after:w-full after:transition-[500ms] cursor-pointer relative">
+            self-development
+          </li>
+          <li className="after:content-[''] after:absolute after:bg-light after:h-[3px] after:rounded-lg after:w-0 left-0 after:bottom-[-10px] hover:after:w-full after:transition-[500ms] cursor-pointer relative">
+            mental health
+          </li>
         </ul>
 
         {data.map((post, index) => (
