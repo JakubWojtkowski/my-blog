bd5e6d6c94752b9f08762a933748e56f735519f8
wojtko <j.w2001@onet.pl>
2024-03-01 22:44:04 +0100

    Minor change

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -5,7 +5,7 @@ export default function Navbar() {
   return (
     <nav className="w-full max-w-4xl mx-auto flex justify-between py-8">
       <Link href={"/"} className="text-3xl tracking-tighter font-bold relative">
-        improveu{" "}
+        improveu
         <span className="absolute text-base bottom-[-24px] tracking-tight right-0 border-2 dark:border-light border-dark rounded-lg px-2">
           blog
         </span>
