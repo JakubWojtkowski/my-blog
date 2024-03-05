b703f6a3cb531b0dd6393a9e6fcb0c846e53fa6e
wojtko <j.w2001@onet.pl>
2024-03-05 22:33:56 +0100

    Animation challenges

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,19 +1,34 @@
+// "use client";
+
 import { ArrowDownRight } from "lucide-react";
 import Image from "next/image";
 import Marquee from "react-fast-marquee";
+import { motion, useInView } from "framer-motion";
+import { slideUp, opacity } from "../anim";
+import { useRef } from "react";
 
 export default function Hero() {
+  // const container = useRef(null);
+  // const isInView = useInView(container);
+  // const phrase = "Built yourself today with improveu";
+
   return (
     <div className="w-full flex flex-col max-w-4xl mx-auto py-4 h-full gap-4 sm:gap-8 px-8 lg:px-0">
-      <h1 className="font-bold text-center sm:text-left text-5xl sm:text-7xl md:text-8xl flex-1">
+      <h1 className="font-bold text-center sm:text-left text-5xl sm:text-7xl md:text-8xl flex-1 z-10">
         Built yourself today with
         <span className="border-b-8 dark:border-light border-dark italic tracking-tighter font-normal">
           {" "}
           improveu
         </span>
+        {/* {
+          phrase.split(" ").map((word, index) => {
+            return <span key={index}><motion.span variants={slideUp}
+              initial="initial" animate={isInView ? "open" : "closed"}>{word}</motion.span></span>
+          })
+        } */}
       </h1>
 
-      <div className="grid gap-4 sm:gap-8 grid-cols-1 sm:grid-cols-2 justify-items-center sm:justify-items-stretch mt-8 sm:mt-6">
+      <div data-scroll data-scroll-speed="0.3" className="grid gap-4 sm:gap-8 grid-cols-1 sm:grid-cols-2 justify-items-center sm:justify-items-stretch mt-8 sm:mt-6">
         <p className="text-center text-base sm:text-left sm:text-lg leading-8">
           The <span className="italic">improveu</span> is your destination for<br></br>
           self-development and personal growth
@@ -23,7 +38,7 @@ export default function Hero() {
           <Image
             src="https://i.pinimg.com/originals/5a/4d/dc/5a4ddcf0f283e07719d52b81a13f201b.jpg"
             alt="blog's image"
-            width={220}
+            width={210}
             height={210}
             sizes="(max-width: 425px) 240px, 240px"
             className="rounded-md cursor-pointer sm:w-[230px]"
--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ import Hero from "./components/Hero";
 import Footer from "./components/Footer";
 import "./globals.css";
 import { ArrowDownRight } from "lucide-react";
-import Menu from "./components/Menu";
+// import Menu from "./components/Menu";
 import DropdownMenu from "./components/DropdownMenu";
 
 export const revalidate = 30;
@@ -34,13 +34,13 @@ export default async function Home() {
     <div className="grid grid-cols-1 mt-4 gap-12">
       <Hero />
 
-      <div className="w-full max-w-4xl mx-auto pt-4 px-8 lg:px-0">
-        <h2 className="text-center font-bold tracking-tighter text-4xl md:text-5xl pb-8">
-          Latest posts
+      <div className="w-full max-w-4xl mx-auto md:pt-8 px-8 lg:px-0 min-h-[100vh]">
+        <h2 className="text-center font-bold tracking-tighter text-4xl md:text-5xl pb-2">
+          Articles
         </h2>
 
-        <Menu />
-        <DropdownMenu />
+        {/* <Menu /> */}
+        {/* <DropdownMenu /> */}
 
         {data.map((post, index) => (
           <Card
@@ -55,9 +55,12 @@ export default async function Home() {
               className="rounded-lg h-[200px] object-cover"
             />
             <CardContent className="flex flex-col justify-end gap-1 sm:gap-2 flex-1 p-0">
-              <p className="text-base text-gray-600 dark:text-gray-300">
+              <p className="text-base text-gray-600 dark:text-gray-400 font-semibold">
                 Feb 21, 2024
               </p>
+
+              <div className="text-xs tracking-wide my-2"><span className="rounded-xl bg-slate-100 text-slate-500 dark:text-slate-400 dark:bg-gray-800 px-2 py-2">mental health</span></div>
+
               <h3 className="text-2xl font-bold cursor-pointer">
                 {post.title.length >= 36
                   ? post.title.slice(0, 36).concat("...")
