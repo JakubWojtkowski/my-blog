24db21c3a96caf0dfe34eca013ab4db884b8b3ee
wojtko <j.w2001@onet.pl>
2024-03-15 22:46:04 +0100

    Code remove

--- app/components/Menu.tsx
+++ app/components/Menu.tsx
@@ -1,18 +1,5 @@
 export default function Menu() {
     return (
-        <ul className="flex-1 sm:flex justify-center items-center gap-4 text-base hidden">
-            <li className="after:content-[''] after:absolute after:bg-light after:h-[3px] after:rounded-lg after:w-0 left-0 after:bottom-[-10px] hover:after:w-full after:transition-[500ms] cursor-pointer relative">
-                all
-            </li>
-            <li className="after:content-[''] after:absolute after:bg-light after:h-[3px] after:rounded-lg after:w-0 left-0 after:bottom-[-10px] hover:after:w-full after:transition-[500ms] cursor-pointer relative">
-                nutrition
-            </li>
-            <li className="after:content-[''] after:absolute after:bg-light after:h-[3px] after:rounded-lg after:w-0 left-0 after:bottom-[-10px] hover:after:w-full after:transition-[500ms] cursor-pointer relative">
-                self-development
-            </li>
-            <li className="after:content-[''] after:absolute after:bg-light after:h-[3px] after:rounded-lg after:w-0 left-0 after:bottom-[-10px] hover:after:w-full after:transition-[500ms] cursor-pointer relative">
-                mental health
-            </li>
-        </ul>
+      
     )
 }
