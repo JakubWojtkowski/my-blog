d74e8603f1eecdd1249ed34f4fc304d2914fa9bf
wojtko <j.w2001@onet.pl>
2024-04-02 21:14:12 +0200

    Fix hero section issues

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -25,7 +25,7 @@ export default function Hero({ post }: Props) {
   }, []);
 
   return (
-    <div className="sticky left-0 top-0 pt-6 sm:pt-0 -z-10 w-full flex flex-col max-w-4xl mx-auto pb-4 h-full gap-4 sm:gap-8 px-8 lg:px-0 z-10">
+    <div className="sticky left-0 top-0 pt-6 sm:pt-0 w-full flex flex-col max-w-4xl mx-auto pb-4 h-full gap-4 sm:gap-8 px-8 lg:px-0 z-10">
       <Reveal>
         <div className="py-3">
           <h1 className="font-bold text-center sm:text-left text-5xl sm:text-7xl md:text-8xl flex-1 z-10">
